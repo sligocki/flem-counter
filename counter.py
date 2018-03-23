647dc3098ebae54ba8ba0a5980e1b1581f38f556
Shawn Ligocki <sligocki@gmail.com>
2018-03-23 09:25:03 -0400

    Unshard the counter. That is using up my Datastore Free quota super-fast!

--- counter.py
+++ counter.py
@@ -15,34 +15,40 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 
-NUM_SHARDS = 100
-
 class BasicCounter(ndb.Model):
     count = ndb.IntegerProperty(default=0)
 
-# TODO: Generalize this so that we could have multiple counters.
-def get_count():
-    total = 0
-    for counter in BasicCounter.query():
-        total += counter.count
-    return total
+# NUM_SHARDS = 100
+
+# # TODO: Generalize this so that we could have multiple counters.
+# def get_count():
+#     total = 0
+#     for counter in BasicCounter.query():
+#         total += counter.count
+#     return total
+
+# def increment_count():
+#     shard_index = random.randrange(NUM_SHARDS)
+#     shard_name = str(shard_index)
+#     get_and_increment(shard_name)
+
+def get_count(id):
+    counter = BasicCounter.get_or_insert(id)
+    return counter.count
 
 @ndb.transactional
-def increment_count():
-    shard_index = random.randrange(NUM_SHARDS)
-    shard_name = str(shard_index)
-    shard_counter = BasicCounter.get_by_id(shard_name)
-    if shard_counter is None:
-        shard_counter = BasicCounter(id=shard_name)
-    shard_counter.count += 1
-    shard_counter.put()
+def get_and_increment(id):
+    counter = BasicCounter.get_or_insert(id)
+    counter.count += 1
+    counter.put()
+    return counter.count
 
+FLEM_COUNTER_ID = "flem-counter"
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         # TODO: Increment after to avoid blocking page?
-        increment_count()
-        flem_count = get_count()
+        flem_count = get_and_increment(FLEM_COUNTER_ID)
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render({
             'flem_count': flem_count,
